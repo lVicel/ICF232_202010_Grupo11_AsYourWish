94678033e371f3fe54d5b6ebf408a21416b1f21b
Cristobal Olave <CXM.242@gmail.com>
2020-07-16 18:42:49 -0400

    Prototipo de Aplicacion Añadido

--- PaginaPrincipal/AsYouWish/App_Base/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Alumno
-
-class AlumnoForm(ModelForm):
-	class Meta:
-		model = Alumno
-		fields = ['RUT', 'Nombre', 'Email']
